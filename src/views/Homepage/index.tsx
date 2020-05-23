ece3eb581b8329c6744be3e5bce90baad4654ff3
Magdalena Brzozowska <megi1312@gmail.com>
2020-05-23 16:27:20 +0200

    add fade transition

--- src/views/Homepage/index.tsx
+++ src/views/Homepage/index.tsx
@@ -3,11 +3,16 @@ import React from "react";
 import { useAuth } from "../../contexts/authContext";
 import LoginForm from "../../components/LoginForm";
 import UserPanel from "../../components/UserPanel";
+import { Fade } from "@material-ui/core";
 
 const Homepage: React.FC = () => {
   const { user } = useAuth();
 
-  return user ? <UserPanel /> : <LoginForm />;
+  return (
+    <Fade in={true} timeout={1000} disableStrictModeCompat={true}>
+      <div>{user ? <UserPanel /> : <LoginForm />}</div>
+    </Fade>
+  );
 };
 
 export default Homepage;
